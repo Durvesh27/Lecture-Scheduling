01da9c69f74c34298c198198a6fc0591a2c0e44a
Durvesh27 <132134129+Durvesh27@users.noreply.github.com>
2024-01-02 22:02:33 +0530

    Updated

--- lecture-scheduling/src/Components/Api Config/index.js
+++ lecture-scheduling/src/Components/Api Config/index.js
@@ -3,12 +3,12 @@ import axios from "axios";
 const token = JSON.parse(localStorage.getItem("Token"));
 if (token) {
   var api = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "https://lecture-scheduling-backend-41oh.onrender.com",
     headers: { Authorization: `Bearer${token}` },
   });
 } else {
   var api = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "https://lecture-scheduling-backend-41oh.onrender.com",
   });
 }
 export default api;
